b35a23a5bab4123f946fb6525498f61aa9f76bf6
ofuchia <o.akpom@tcu.edu>
2023-09-14 22:31:30 -0500

    Initial commit

---
+++ src/main/java/edu/tcu/cs/storeonline/StoreonlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.storeonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/storeonline/StoreonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.storeonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
